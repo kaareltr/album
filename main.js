6ab7e36c108c52b566c225bf5020ce9a26fee7b7
Kaarel Truu <kaareltruu@yahoo.co.uk>
2023-07-20 17:28:53 +0300

    Images and style fixes

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ const book = document.querySelector("#book");
 const paper1 = document.querySelector("#p1");
 const paper2 = document.querySelector("#p2");
 const paper3 = document.querySelector("#p3");
+const paper4 = document.querySelector("#p4");
+const paper5 = document.querySelector("#p5");
 
 prevBtn.addEventListener("click", goPrevPage);
 nextBtn.addEventListener("click", goNextPage);
 
 let currentLocation = 1;
-let numOfPapers = 3;
+let numOfPapers = 5;
 let maxLocation = numOfPapers + 1;
 
 function openBook() {
@@ -44,6 +46,14 @@ function goNextPage() {
       case 3:
         paper3.classList.add("flipped");
         paper3.style.zIndex = 3;
+        break;
+      case 4:
+        paper4.classList.add("flipped");
+        paper4.style.zIndex = 4;
+        break;
+      case 5:
+        paper5.classList.add("flipped");
+        paper5.style.zIndex = 5;
         closeBook();
         break;
       default:
@@ -59,16 +69,24 @@ function goPrevPage() {
       case 2:
         closeBook(true);
         paper1.classList.remove("flipped");
-        paper1.style.zIndex = 3;
+        paper1.style.zIndex = 5;
         break;
       case 3:
         paper2.classList.remove("flipped");
-        paper2.style.zIndex = 2;
+        paper2.style.zIndex = 4;
         break;
       case 4:
-        openBook();
         paper3.classList.remove("flipped");
-        paper3.style.zIndex = 1;
+        paper3.style.zIndex = 3;
+        break;
+      case 5:
+        paper4.classList.remove("flipped");
+        paper4.style.zIndex = 2;
+        break;
+      case 6:
+        openBook();
+        paper5.classList.remove("flipped");
+        paper5.style.zIndex = 1;
         break;
       default:
         throw new Error("unknown state");
